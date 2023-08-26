70e6ce476e698a436e15e4e74d2ff2542f4cb4a1
Divyasai3339 <141727018+Divyasai3339@users.noreply.github.com>
2023-08-26 20:18:07 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* ======================= typing animation ======================= */
 var typed = new Typed(".typing", {
-    strings:["","Web Designer","Web Developer","Web Designer","Web Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
+    strings:["","Web Designer","Web Developer"],
+    typeSpeed:60,
+    BackSpeed:40,
     loop:true
 })
