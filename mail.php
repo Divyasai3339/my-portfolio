038b82b436eb00714b744f6a61729c6257ece005
Divyasai3339 <141727018+Divyasai3339@users.noreply.github.com>
2023-08-26 13:46:32 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,10 +4,10 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$to = "akshayfacialplasticsurgeries@gmail.com";
+$to = "divyasai3339@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone =" . $phone. "\r\n Message =" . $message;
-$headers = "From: akshayfacialplasticsurgeries@gmail.com";
+$headers = "From: divyasai3339@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
